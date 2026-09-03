80fee24eb8f3e7f3814e312f6e4d29a82d4ec52f
Abdullah Shahzad <abdullahshahzadnjr@gmail.com>
2026-09-03 07:38:08 +0500

    update code

--- Lab 2/syntax.py
+++ Lab 2/syntax.py
@@ -1,1 +1,12 @@
-print("Hello world")
+class Student:
+    pass
+s1=Student()
+s2=Student()
+
+class Myclass:
+    x=5
+    def method_one(self):
+        print("this is method one")
+
+m1=Myclass()
+print(m1.method_one)
